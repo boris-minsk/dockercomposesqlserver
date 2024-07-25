e4d3faccec598663fcd60c09c4204bb9363abb98
boris-minsk <40657038+boris-minsk@users.noreply.github.com>
2024-07-25 12:48:19 +0200

    Working code from Az DevOps

--- SqlContainerDemo/web/Program.cs
+++ SqlContainerDemo/web/Program.cs
@@ -26,9 +26,12 @@ namespace web
 
             app.UseAuthorization();
 
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
 
             app.Run();
         }
